40f3f2122eee3beab1031f23ec63cb9db01a2d27
Fábio Maia <fabiu.maia@gmail.com>
2018-04-01 23:42:23 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # docker-shreddit
+
+Automate running Shreddit (within a Docker container) so I don't have to reread the instructions whenever I run it every once in a while.
+
+## Run
+
+```
+docker run -it faviouz/shreddit
+```
+
+## Build
+
+```
+docker build -t shreddit .
+docker tag shreddit faviouz/shreddit:latest
+docker push faviouz/shreddit:latest
+```
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Ask for account details
+echo "Get account details at https://www.reddit.com/prefs/apps/..."
+read -p "client_id: " client_id
+read -p "client_secret: " client_secret
+read -p "username: " username
+read -p "password: " password
+
+# Generate config files
+shreddit -g
+
+# Write config file
+cat > praw.ini <<EOL
+[default]
+client_id=$client_id
+client_secret=$client_secret
+username=$username
+password=$password
+EOL
+
+# Wipe
+shreddit
+
+# Cleanup
+rm praw.ini shreddit.yml
+
